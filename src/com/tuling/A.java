6515f1c42049f640e875c7eefc1695afa1b4e219
fred <2337362521@qq.com>
2019-10-24 19:45:56 +0800

    在dev中更新了

---
+++ src/com/tuling/A.java
@@ -0,0 +1,7 @@
+package com.tuling;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("我是dev中创建的类");
+    }
+}
--- src/com/tuling/Main.java
+++ src/com/tuling/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public void show(){
 
         System.out.println("show...");
+        System.out.println("我是dev中添加的一句话");
 
     }
 }
